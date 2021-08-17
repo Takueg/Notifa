af460bb8a57b0706917edbc9d06d17150ffc0700
Jorge Cases <jcasoya@gmail.com>
2021-08-17 19:51:53 +0200

    index backend complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,10 @@
 class PostsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :show, :index]
+
+  def index
+    @posts = Post.all
+  end
+
   def show
     @posts = Post.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :posts, only: [:show]
+  resources :posts, only: [:index, :show]
   devise_for :users
 
 
   root to: 'pages#home'
+  get 'index', to: 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
