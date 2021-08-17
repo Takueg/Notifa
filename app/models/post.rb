6c89ef9828c3d4df916008f415733d469711b326
Jorge Cases <jcasoya@gmail.com>
2021-08-17 20:12:46 +0200

    long lat columns add and  migration to posts table

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   has_many :notifications
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_14_093054) do
+ActiveRecord::Schema.define(version: 2021_08_17_181029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 2021_08_14_093054) do
     t.string "company"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "searches", force: :cascade do |t|
