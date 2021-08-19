f8cce3efe6790617356b0dac18cc212670eb10f9
Takudzwa Gwindingwi <takueg@gmail.com>
2021-08-19 19:46:25 +0200

    adding input to home and search create controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,5 +2,10 @@ class SearchesController < ApplicationController
   def new
     # @city = params[:city]
     @search = Search.new
+    @search.city = params[:query]
+  end
+
+  def create
+    @search = Search.new(search_params)
   end
 end
