6008b1cdcc9bd377614b2ac0dfac1d6b9b71669d
Takudzwa Gwindingwi <takueg@gmail.com>
2021-08-21 17:18:43 +0200

    adding pg_search

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,11 @@ class Post < ApplicationRecord
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  include PgSearch::Model
+  pg_search_scope :search_posts,
+    against: [ :price, :size, :room, :category, :date_posted, :address ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
